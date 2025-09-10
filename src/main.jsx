10ab31c1a2892f24e41cee040e10d8072802890d
JuanCamilx <juankatiks.ff@gmail.com>
2025-09-10 07:17:32 -0500

    subiendo arreglos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+import FormularioDeDatos from './FormularioDeDatos.jsx';
+// import App from './Guias.jsx'   // 👈 lo dejas comentado por ahora
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FormularioDeDatos />   {/* 👈 aquí renderizas tu formulario */}
   </StrictMode>,
-)
+);
